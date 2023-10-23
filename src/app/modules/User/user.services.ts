79d9a29e4e229cc21eb99aff02f8feacfd327124
Shariful Islam <sharifsharifulislam061@gmail.com>
2023-10-23 22:00:46 +0600

    User service added - create User service added - get all User service added - get a specific User service added - update a user service added - delete a user service added

---
+++ src/app/modules/User/user.services.ts
@@ -0,0 +1,37 @@
+import IUser from './user.interface'
+import User from './user.model'
+
+const createUser = async (user: IUser): Promise<IUser> => {
+  const result = await User.create(user)
+  return result
+}
+const getAllUsers = async (): Promise<IUser[]> => {
+  const result = await User.find({})
+  return result
+}
+const getUser = async (id: string): Promise<IUser | null> => {
+  const result = await User.findById(id)
+  return result
+}
+const updateUser = async (
+  id: string,
+  updateData: Partial<IUser>,
+): Promise<IUser | null> => {
+  const result = await User.findByIdAndUpdate(
+    id,
+    { $set: updateData },
+    { new: true },
+  )
+  return result
+}
+const deleteUser = async (id: string): Promise<IUser | null> => {
+  const result = await User.findByIdAndDelete(id)
+  return result
+}
+export const userServices = {
+  createUser,
+  getAllUsers,
+  getUser,
+  updateUser,
+  deleteUser,
+}
